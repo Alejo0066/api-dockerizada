56b6fa39252ca4e3e415800edaaf2646a849da87
Alejo0066 <asalazarc@itc.edu.co>
2025-03-11 12:01:34 -0500

    Update database.py
    ACTUALIZACION URL

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # URL de conexión a PostgreSQL (ajusta con tus credenciales)
-DATABASE_URL = "postgresql://taller_user:123456@db:5432/taller_db"
+DATABASE_URL = "postgresql://taller_db_x0h8_user:n4Bj2iFgT8NmMUhxTOMScLRoLvpwMEsL@dpg-cv86n8rqf0us73f682a0-a/taller_db_x0h8"
 
 # Configurar la base de datos
 engine = create_engine(DATABASE_URL)
